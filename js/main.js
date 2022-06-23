7d50775f0cdead202a1c135b8c8691e640e62b3e
ErJump <fedelijunior@gmail.com>
2022-06-23 14:29:26 +0200

    Sets for loop limit to difficulty value

--- js/main.js
+++ js/main.js
@@ -10,11 +10,12 @@ function generateNewBox (){
 
 //creo l'evento per il quale si genera la griglia
 startButton.addEventListener('click', function(){
-    console.log(difficulty.value)
+    const difficultyValue = difficulty.value
+    console.log(difficultyValue);
     //aggiungo un reset per non generare box infiniti
     gridParent.innerHTML = '';
     //ciclo for per popolare la griglia con i nuovi box
-    for (let i = 1; i < 101; i++){
+    for (let i = 1; i < difficultyValue; i++){
         let newDiv = generateNewBox();
         newDiv.innerHTML = i;
         gridParent.append(newDiv);
