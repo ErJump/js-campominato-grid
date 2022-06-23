759d63b623eb58c65e78caf4015c7df8ed5e422a
ErJump <fedelijunior@gmail.com>
2022-06-23 14:00:35 +0200

    Adds functionality to start button

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const gridParent = document.getElementById('grid-parent');
-
+const startButton = document.getElementById('start-button');
 //creo una funzione per generare un nuovo div con classe box
 function generateNewBox (){
     let item = document.createElement('div');
@@ -7,9 +7,14 @@ function generateNewBox (){
     return item
 }
 
-//ciclo for per popolare la griglia con i nuovi box
-for (let i = 1; i < 101; i++){
-    let newDiv = generateNewBox();
-    newDiv.innerHTML = i;
-    gridParent.append(newDiv);
-}
+//creo l'evento per il quale si genera la griglia
+startButton.addEventListener('click', function(){
+    //aggiungo un reset per non generare box infiniti
+    gridParent.innerHTML = '';
+    //ciclo for per popolare la griglia con i nuovi box
+    for (let i = 1; i < 101; i++){
+        let newDiv = generateNewBox();
+        newDiv.innerHTML = i;
+        gridParent.append(newDiv);
+    }
+})
